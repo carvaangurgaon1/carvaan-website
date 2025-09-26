bf97031f0082dbcfc7781ddd541523566621639f
carvaangurgaon1 <carvaan.eureka2025@gmail.com>
2025-09-26 11:08:09 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,27 @@
+import Hero from "./components/Hero";
+import HowItWorks from "./components/HowItWorks";
+import FeaturedTrips from "./components/FeaturedTrips";
+import WhyCarvaan from "./components/WhyCarvaan";
+import CommunityStories from "./components/CommunityStories";
+import TripBuilder from "./components/TripBuilder";
+import BlogHub from "./components/BlogHub";
+import CarvaanCoins from "./components/CarvaanCoins";
+import CaptainsAgents from "./components/CaptainsAgents";
+import FinalCTA from "./components/FinalCTA";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to Carvaan 🚐✨</h1>
-      <p>This is your homepage.</p>
-    </div>
+    <main>
+      <Hero />
+      <HowItWorks />
+      <FeaturedTrips />
+      <WhyCarvaan />
+      <CommunityStories />
+      <TripBuilder />
+      <BlogHub />
+      <CarvaanCoins />
+      <CaptainsAgents />
+      <FinalCTA />
+    </main>
   );
 }
