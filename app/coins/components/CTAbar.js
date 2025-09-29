3a182eee581a4ee964583b5ef4939ae8d7c65833
carvaangurgaon1 <carvaan.eureka2025@gmail.com>
2025-09-29 14:54:16 +0530

    Create CTAbar.js

---
+++ app/coins/components/CTAbar.js
@@ -0,0 +1,10 @@
+export default function CTAbar() {
+  return (
+    <div className="fixed bottom-0 left-0 right-0 bg-purple-700 text-white py-4 px-6 flex justify-between items-center z-50">
+      <p className="font-semibold">👉 Travel more. Earn more. Redeem more.</p>
+      <button className="bg-yellow-400 text-black px-6 py-2 rounded font-bold">
+        Start Collecting Coins
+      </button>
+    </div>
+  );
+}
