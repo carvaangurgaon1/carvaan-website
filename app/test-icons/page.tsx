fe062235c12fa8cb35e19a97025431dd5157853a
carvaangurgaon1 <carvaan.eureka2025@gmail.com>
2025-10-13 22:39:07 +0530

    Create page.tsx

---
+++ app/test-icons/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { FiPackage } from "react-icons/fi";
+
+export default function TestIcons() {
+  return (
+    <div className="p-10 text-center">
+      <FiPackage className="text-5xl text-purple-600" />
+      <p>React Icons test successful ✅</p>
+    </div>
+  );
+}
