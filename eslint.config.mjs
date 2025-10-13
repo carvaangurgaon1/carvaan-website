bd8038ca39e2dc6a4cbc362cce9f866834f1d1bf
carvaangurgaon1 <carvaan.eureka2025@gmail.com>
2025-10-13 23:02:57 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,8 @@
-// eslint.config.js
 import nextPlugin from 'eslint-config-next';
 
 export default [
   {
     ignores: ['node_modules', '.next', 'dist'],
-    // skip the @rushstack patch (works around the error)
-    plugins: [],
   },
   ...nextPlugin(),
 ];
