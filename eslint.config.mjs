3e120456565c71e872f05e3e8765eec204bf32c7
carvaangurgaon1 <carvaan.eureka2025@gmail.com>
2025-10-13 22:54:13 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,9 @@
+// eslint.config.js
 import nextPlugin from 'eslint-config-next';
 
 export default [
   {
+    // Files/folders to ignore
     ignores: ['node_modules', '.next', 'dist'],
   },
   ...nextPlugin(),
